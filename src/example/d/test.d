2e2171cca48e6d784419f2185b5a1a6be9a02079
Eric R. Schulz <eric@ers35.com>
2016-07-30 17:11:49 -0400

    add example/d/test.d

---
+++ src/example/d/test.d
@@ -0,0 +1,33 @@
+import modserver;
+import core.stdc.string;
+
+extern (C) int run(servlet *s)
+{
+  const char *arg = get_arg(s, "arg");
+
+  const char *method = get_method(s);
+  assert(strcmp(method, "GET") == 0);
+  
+  const char *val0 = get_header(s, "User-Agent");
+  const char *val1 = get_header(s, "user-agent");
+  if (val0 && val1)
+  {
+    assert(strcmp(val0, val1) == 0);
+    rprintf(s, val0);
+  }
+  
+  set_status(s, 200);
+  
+  set_header(s, "Content-Type", "text/plain; charset=UTF-8");
+  
+  if (arg)
+  {
+    rprintf(s, arg);
+  }
+  
+  rprintf(s, "The number is: %u\n", 42);
+  
+  rflush(s);
+    
+  return 0;
+}
